4a8ac9a90d892bfd5ddd328ea9c9e92d59ec0efc
Matthew Biskas <matthewbiskas@gmail.com>
2026-05-19 15:14:00 +0200

    test cases for conatiner

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,16 @@
 <?php
 
+use TheThunderTurner\Docker\Docker;
 use TheThunderTurner\Docker\Tests\TestCase;
 
-uses(TestCase::class)->in(__DIR__);
+uses(TestCase::class)
+    ->beforeEach(function (): void {
+        $this->docker = new Docker;
+        $this->cleanup = [];
+    })
+    ->afterEach(function (): void {
+        foreach ($this->cleanup as $id) {
+            $this->docker->containers()->remove($id, force: true);
+        }
+    })
+    ->in(__DIR__);
